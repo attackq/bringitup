0fe670bd3c6db22bc1d1887a15f7519f242f8005
Alexander Panasik <rastpk@gmail.com>
2021-12-25 13:30:15 +0300

    added difference.js

---
+++ src/js/modules/difference.js
@@ -0,0 +1,43 @@
+export default class Difference {
+    constructor(oldOfficer, newOfficer, items) {
+        this.oldOfficer = document.querySelector(oldOfficer);
+        this.newOfficer = document.querySelector(newOfficer);
+        this.oldItems = this.oldOfficer.querySelectorAll(items);
+        this.newItems = this.newOfficer.querySelectorAll(items);
+        this.items = items;
+        this.oldCounter = 0;
+        this.newCounter = 0;
+    }
+
+    bindTriggers(off, items, count) {
+        off.querySelector('.plus').addEventListener('click', () => {
+            if (count !== items.length - 2) {
+                items[count].style.display = 'flex';
+                items[count].classList.add('fadeIn');
+                count++;
+            } else {
+                items[count].style.display = 'flex';
+                items[items.length - 1].remove();
+            }
+        });
+
+    }
+
+    hideItems(obj) {
+        obj.forEach((item, i, arr) => {
+            if (i !== arr.length - 1) {
+                item.style.display = 'none';
+                item.classList.add('animated');
+            }
+        });
+    }
+
+
+
+    init() {
+        this.hideItems(this.oldItems);
+        this.hideItems(this.newItems);
+        this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+        this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
+    }
+}
